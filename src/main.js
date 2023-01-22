2028b5f33e0bceb1531a1253e56ef445dcd37944
Omar Diop <accounts@omardiop.com>
2023-01-22 15:08:31 +0100

    Persisted session data if popup is closed

--- src/main.js
+++ src/main.js
@@ -1,49 +1,81 @@
+document.addEventListener("DOMContentLoaded", () => {
+  chrome.storage.session.get("savedStade").then(({ savedStade }) => {
+    const { productDescription, headlines, bodies } = savedStade
+
+    console.log("Description: " + productDescription)
+    console.log("Headlines: " + headlines)
+    console.log("Bodies: " + bodies)
+
+    setDescription(productDescription)
+    setHeadlines(headlines)
+    setBodies(bodies)
+  })
+})
+
 document.addEventListener(
   "click",
   function (event) {
-    if (!event.target.matches("#submit-button")) return
-
     event.preventDefault()
-    const headlinesLoader = document.querySelector(".headlines-loader")
-    const bodiesLoader = document.querySelector(".bodies-loader")
+
     const headlinesResult = document.querySelector(".headlines-result")
     const bodiesResult = document.querySelector(".bodies-result")
-    headlinesLoader.style.display = "block"
-    bodiesLoader.style.display = "block"
+    const headlinesLoader = document.querySelector(".headlines-loader")
+    const bodiesLoader = document.querySelector(".bodies-loader")
 
-    const description = document.getElementById("textarea").value
-    postData("https://api.openai.com/v1/completions", {
-      model: "text-davinci-003",
-      prompt: `Trova cinque headlines con angle diversi per il seguente prodotto da sponsorizzare tramite Facebook Ads:\n\nProdotto:${description}. Il risultato dev'essere un'elenco puntato.`,
-      temperature: 0.5,
-      max_tokens: 256,
-      top_p: 1.0,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-    }).then((data) => {
-      console.log(data)
-      const element = document.querySelector("#headlines")
-      element.innerText = data.choices[0].text
+    if (event.target.matches("#submit-button")) {
+      headlinesLoader.style.display = "block"
+      bodiesLoader.style.display = "block"
+      headlinesResult.style.display = "none"
+      bodiesResult.style.display = "none"
+
+      const description = document.getElementById("textarea").value
+
+      let session = {
+        productDescription: description,
+        headlines: null,
+        bodies: null,
+      }
+
+      postData("https://api.openai.com/v1/completions", {
+        model: "text-davinci-003",
+        prompt: `Trova cinque headlines con angle diversi per il seguente prodotto da sponsorizzare tramite Facebook Ads:\n\nProdotto:${description}. Il risultato dev'essere un'elenco puntato.`,
+        temperature: 0.7,
+        max_tokens: 256,
+        top_p: 1.0,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+      }).then((data) => {
+        console.log(data)
+        session = { ...session, headlines: data.choices[0].text }
+        chrome.storage.session.set({
+          savedStade: session,
+        })
+        setHeadlines(data.choices[0].text)
+      })
+
+      postData("https://api.openai.com/v1/completions", {
+        model: "text-davinci-003",
+        prompt: `Trova tre body text con angle diversi per il seguente prodotto da sponsorizzare tramite Facebook Ads:\n\nProdotto:${description}. Il risultato dev'essere un elenco puntato.`,
+        temperature: 0.7,
+        max_tokens: 1000,
+        top_p: 1.0,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+      }).then((data) => {
+        console.log(data)
+        session = { ...session, bodies: data.choices[0].text }
+        chrome.storage.session.set({
+          savedStade: session,
+        })
+        setBodies(data.choices[0].text)
+      })
+    } else if (event.target.matches("#reset-button")) {
       headlinesLoader.style.display = "none"
-      headlinesResult.style.display = "block"
-    })
-
-    postData("https://api.openai.com/v1/completions", {
-      model: "text-davinci-003",
-      prompt: `Trova tre body text con angle diversi per il seguente prodotto da sponsorizzare tramite Facebook Ads:\n\nProdotto:${description}. Il risultato dev'essere un elenco puntato.`,
-      temperature: 0.5,
-      max_tokens: 1000,
-      top_p: 1.0,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-    }).then((data) => {
-      console.log(data)
-
-      const element = document.querySelector("#body-texts")
-      element.innerText = data.choices[0].text
       bodiesLoader.style.display = "none"
-      bodiesResult.style.display = "block"
-    })
+      headlinesResult.style.display = "none"
+      bodiesResult.style.display = "none"
+      document.getElementById("textarea").value = ""
+    }
   },
   false
 )
@@ -60,4 +92,29 @@ async function postData(url = "", data = {}) {
     body: JSON.stringify(data),
   })
   return response.json()
+}
+
+function setDescription(description) {
+  if (!description) return
+  document.getElementById("textarea").value = description
+}
+
+function setHeadlines(headlines) {
+  if (!headlines) return
+  const headlinesResult = document.querySelector(".headlines-result")
+  const headlinesLoader = document.querySelector(".headlines-loader")
+  const headlinesDiv = document.querySelector("#headlines")
+  headlinesDiv.innerText = headlines
+  headlinesLoader.style.display = "none"
+  headlinesResult.style.display = "block"
+}
+
+function setBodies(bodies) {
+  if (!bodies) return
+  const bodiesResult = document.querySelector(".bodies-result")
+  const bodiesLoader = document.querySelector(".bodies-loader")
+  const bodiesDiv = document.querySelector("#body-texts")
+  bodiesDiv.innerText = bodies
+  bodiesLoader.style.display = "none"
+  bodiesResult.style.display = "block"
 }
